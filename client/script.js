4b5d22075cda3a3381daacc688dc20291c170f1e
Danielle Muhlenberg <dmuhlenb@uci.edu>
2020-02-02 08:55:56 -0800

    changed colors of different users

--- client/script.js
+++ client/script.js
@@ -2,11 +2,8 @@ let calendarResponse;
 let globalCalendar = [];
 
 $(function() {
-
     document.getElementById("submit").onclick = function loadCal() {
-        document.getElementById("calendar").classList.remove('hide');
-        // document.getElementById("form").classList.add('collapse');
-        
+        document.getElementById("calendar").classList.remove('hide');        
         document.getElementById("form").classList.add('garbage');
         document.getElementById("groups").classList.remove('garbage');
         var username = document.getElementById('calKey').value;
@@ -15,18 +12,17 @@ $(function() {
             "crossDomain": true,
             "url": "http://localhost:3000/schedule?user="+username,
             "method": "GET"
-          }
+        }
           
           $.ajax(settings).done(function (response) {
                 let calendarId = response.calendarID;
-                        // send username to db and get back cal id & set to
+                let displayId = response.displayID;
                 prefix = 'https://www.googleapis.com/calendar/v3/calendars/'
 
                 var url = prefix + calendarId + '/events?key=AIzaSyAzSkGZ7YtaaepNA-r_g7glspLmct-avfs'
 
                 fetch(url).then(response => response.json()).then(response => {
-                    // console.log(JSON.stringify(response));
-                    globalCalendar = parseCalendarResponse(response);
+                    globalCalendar = parseCalendarResponse(response,displayId);
                     return globalCalendar;
                 }).then((globalCalendar) => {
                     var calendarEl = document.getElementById('calendar');
@@ -42,40 +38,7 @@ $(function() {
                     calendar.render();
                 });
           });
-
-        // var settings = {
-        //     "async": true,
-        //     "crossDomain": true,
-        //     "url": "http://localhost:3000/schedule?user="+username,
-        //     "method": "GET"
-        //   }
-          
-        //   $.ajax(settings).done(function (response) {
-        //     console.log(response);
-        //   });
-        
     document.getElementById("join").onclick = function loadCal() {  
-        // document.getElementById("groups").classList.add('garbage');
-        // var username = document.getElementById('calKey').value;
-        // var settings = {
-        //     "async": true,
-        //     "crossDomain": true,
-        //     "url": "http://localhost:3000/schedule?user="+username,
-        //     "method": "GET"
-        //   }
-          
-        //   $.ajax(settings).done(function (response) {
-        //         let calendarId = response.calendarID;
-        //                 // send username to db and get back cal id & set to
-        //         prefix = 'https://www.googleapis.com/calendar/v3/calendars/'
-
-        //         var url = prefix + calendarId + '/events?key=AIzaSyAzSkGZ7YtaaepNA-r_g7glspLmct-avfs'
-
-        //         fetch(url).then(response => response.json()).then(response => {
-        //             // console.log(JSON.stringify(response));
-        //             globalCalendar = parseCalendarResponse(response);
-        //             return globalCalendar;
-        //         }).then((globalCalendar) => {
                     var calendarEl = document.getElementById('calendar');
                     $('#calendar').html('');
     
@@ -85,14 +48,17 @@ $(function() {
                         googleCalendarApiKey: 'AIzaSyAzSkGZ7YtaaepNA-r_g7glspLmct-avfs',
                         eventSources: [
                             {
-                              googleCalendarId: 'vggugcc53gtuvurvfrhjqhnfa8@group.calendar.google.com'
+                              googleCalendarId: 'vggugcc53gtuvurvfrhjqhnfa8@group.calendar.google.com',
+                              className: 'class-one'
+
                             },
                             {
                               googleCalendarId: '2r8ke5q0h5msbiu997l15t4u6g@group.calendar.google.com',
                               className: 'nice-event'
                             },
                             {
-                                googleCalendarId: 't520rbs0es03s180sksph8shds@group.calendar.google.com'
+                                googleCalendarId: 't520rbs0es03s180sksph8shds@group.calendar.google.com',
+                                className: 'class-two'
                             }
                           ],
                           minTime: "06:00:00",
@@ -100,8 +66,6 @@ $(function() {
                     });
             
                     calendar.render();
-                // });
-        //   });
     }
 
 
@@ -128,79 +92,30 @@ $(function() {
 
     $("#groupName").change(function() {
         document.getElementById("join").classList.remove('disabled');
-    });
-
-    
-    
+    });    
     }
 
-// loads the event source for the calendar
-// function getCalendarResponse(){
-//     var calKeyValue = document.getElementById('calKey').value;
+    function parseCalendarResponse(calendarData, displayId) {
+        let calendarDataItems = calendarData['items'];
+        let displayStructure = [];
+        for (let i = 0; i < calendarDataItems.length; i++) {
+            let startDate = new Date(calendarDataItems[i]['start']['dateTime']);
+            let endDate = new Date(calendarDataItems[i]['end']['dateTime']);
+            let title = calendarDataItems[i]['summary'];
 
-//     prefix = 'https://www.googleapis.com/calendar/v3/calendars/'
-
-//     var url = prefix + calKeyValue + '/events?key=AIzaSyAzSkGZ7YtaaepNA-r_g7glspLmct-avfs'
-
-//     fetch(url).then(response => response.json()).then(response => {
-//         // console.log(JSON.stringify(response));
-//         globalCalendar = parseCalendarResponse(response);
-        
-//     });
-//     return calendarResponse;
-// }
-
-
-
-// function getToken(){
-//     fetch('http://localhost:3000/getToken').then(response => response.json()).then(response => {
-//         console.log(JSON.stringify(response));
-//     })
-// }
-
-// vggugcc53gtuvurvfrhjqhnfa8@group.calendar.google.com
-// Returns a structure ready to display on a calendar
-function parseCalendarResponse(calendarData) {
-//     items: Array(2)
-// 0:
-// created: "2020-02-02T04:03:19.000Z"
-// creator: {email: "mohraa14@gmail.com"}
-// end: {dateTime: "2020-02-03T12:30:00-08:00"}
-// etag: ""3161232399916000""
-// htmlLink: "https://www.google.com/calendar/event?eid=NzIyMmM2ZTV1Z2xiaW8yNTl0aWMwbWFxdTUgdmdndWdjYzUzZ3R1dnVydmZyaGpxaG5mYThAZw"
-// iCalUID: "7222c6e5uglbio259tic0maqu5@google.com"
-// id: "7222c6e5uglbio259tic0maqu5"
-// kind: "calendar#event"
-// organizer: {email: "vggugcc53gtuvurvfrhjqhnfa8@group.calendar.google.com", displayName: "Hack Test", self: true}
-// sequence: 0
-// start: {dateTime: "2020-02-03T11:30:00-08:00"}
-// status: "confirmed"
-// summary: "test event"
-// updated: "2020-02-02T04:03:19.958Z
-    let calendarDataItems = calendarData['items'];
-    let displayStructure = [];
-    for (let i = 0; i < calendarDataItems.length; i++) {
-        let startDate = new Date(calendarDataItems[i]['start']['dateTime']);
-        let endDate = new Date(calendarDataItems[i]['end']['dateTime']);
-        let title = calendarDataItems[i]['summary'];
-        let url = calendarDataItems[i]['htmlLink'];
-
-        displayStructure.push(
-            {
-                'start' : startDate.getTime(),
-                'end' : endDate.getTime(),
-                'title' : title,
-                'url' : url
-            }
-        );
-        
-    }
-
+            let url = calendarDataItems[i]['htmlLink'];
+            console.log(displayId);
+            displayStructure.push(
+                {
+                    'start' : startDate.getTime(),
+                    'end' : endDate.getTime(),
+                    'title' : displayId,
+                    'url' : url
+                }
+            );
+            
+        }
     return displayStructure;
-
-
-}
-
-
+    }
 });
 
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 var corsOptions = {
   origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+  optionsSuccessStatus: 200 
 }
 app.use(cors(corsOptions));
 
@@ -46,53 +46,8 @@ app.get('/schedule', function(req, res) {
             res.send(resultObject);
         });
     });
-     /**
-   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
-    // const uri = "mongodb+srv://dbUser:8ojUpKyoLhDsabHr@scheduley-l3rwo.gcp.mongodb.net/test?retryWrites=true&w=majority";
-
-
-    // const client = new MongoClient(uri);
-
-    // try {
-    //     // Connect to the MongoDB cluster
-    //     client.connect();
-
-    //     // Make the appropriate DB calls
-    //     getSchedules(client, userId);
-
-    // } catch (e) {
-    //     console.error(e);
-    // } finally {
-    //     client.close();
-    // }
 });
 
-
-// async function main(){
-//   /**
-//    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-//    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-//    */
-//   const uri = "mongodb+srv://dbUser:8ojUpKyoLhDsabHr@scheduley-l3rwo.gcp.mongodb.net/test?retryWrites=true&w=majority";
-
-
-//   const client = new MongoClient(uri);
-
-//   try {
-//       // Connect to the MongoDB cluster
-//       await client.connect();
-
-//       // Make the appropriate DB calls
-//       await  getSchedules(client);
-
-//   } catch (e) {
-//       console.error(e);
-//   } finally {
-//       await client.close();
-//   }
-// }
 
 async function listDatabases(client){
     const db = client.db("test");
@@ -101,7 +56,6 @@ async function listDatabases(client){
   
     console.log("Databases:");
     console.log(databasesList);
-    // databasesList.databases.forEach(db => console.log(- ${db.name}));
 };
 
 async function getSchedules(client, userId) {
