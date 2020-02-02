cbe8f51be7159fafc98c0075a62764249f825637
Danielle Muhlenberg <dmuhlenb@uci.edu>
2020-02-01 23:42:44 -0800

    Parsing through user data

--- client/script.js
+++ client/script.js
@@ -1,20 +1,88 @@
+let calendarResponse;
 window.onload = function() {
+
     document.getElementById("submit").onclick = function loadCal() {
         document.getElementById("cal").classList.remove('collapse');
         // document.getElementById("form").classList.add('collapse');
         
         document.getElementById("form").classList.add('garbage');
         document.getElementById("groups").classList.remove('garbage');
-        console.log(document.getElementById('calKey').value)
+
+        var calKeyValue = document.getElementById('calKey').value;
+
+
+        prefix = 'https://www.googleapis.com/calendar/v3/calendars/'
+
+        var url = prefix + calKeyValue + '/events?key=AIzaSyAzSkGZ7YtaaepNA-r_g7glspLmct-avfs'
+
+        fetch(url).then(response => response.json()).then(response => {
+            // console.log(JSON.stringify(response));
+            calendarResponse = parseCalendarResponse(response);
+            
+        })
     }
     $("#groupName").change(function() {
         console.log('true')
         document.getElementById("join").classList.remove('disabled');
     });
+    
 }
+let calendar = $("#calendar").calendar(
+    {
+        tmpl_path: "../bower_components/bootstrap-calendar/tmpls/",
+        events_source: calendarResponse
+    }
+);
+
 // function getToken(){
 //     fetch('http://localhost:3000/getToken').then(response => response.json()).then(response => {
 //         console.log(JSON.stringify(response));
 //     })
 // }
+
+// vggugcc53gtuvurvfrhjqhnfa8@group.calendar.google.com
+// Returns a structure ready to display on a calendar
+function parseCalendarResponse(calendarData) {
+//     items: Array(2)
+// 0:
+// created: "2020-02-02T04:03:19.000Z"
+// creator: {email: "mohraa14@gmail.com"}
+// end: {dateTime: "2020-02-03T12:30:00-08:00"}
+// etag: ""3161232399916000""
+// htmlLink: "https://www.google.com/calendar/event?eid=NzIyMmM2ZTV1Z2xiaW8yNTl0aWMwbWFxdTUgdmdndWdjYzUzZ3R1dnVydmZyaGpxaG5mYThAZw"
+// iCalUID: "7222c6e5uglbio259tic0maqu5@google.com"
+// id: "7222c6e5uglbio259tic0maqu5"
+// kind: "calendar#event"
+// organizer: {email: "vggugcc53gtuvurvfrhjqhnfa8@group.calendar.google.com", displayName: "Hack Test", self: true}
+// sequence: 0
+// start: {dateTime: "2020-02-03T11:30:00-08:00"}
+// status: "confirmed"
+// summary: "test event"
+// updated: "2020-02-02T04:03:19.958Z
+    let calendarDataItems = calendarData['items'];
+    let displayStructure = [];
+    for (let i = 0; i < calendarDataItems.length; i++) {
+        let startDate = new Date(calendarDataItems[i]['start']['dateTime']);
+        let endDate = new Date(calendarDataItems[i]['end']['dateTime']);
+        let title = calendarDataItems[i]['summary'];
+        let url = calendarDataItems[i]['htmlLink'];
+
+        displayStructure.push(
+            {
+                'start' : startDate.getTime(),
+                'end' : endDate.getTime(),
+                'title' : title,
+                'url' : url
+            }
+        );
+        
+    }
+
+    return displayStructure;
+
+
+}
+
+
+
 
