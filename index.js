eefe096e8480b6282c997f73624bcecd9a2bbf18
Danielle <30846217+danielle-io@users.noreply.github.com>
2020-12-27 14:06:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ async function main(){
    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
    */
-  const uri = "mongodb+srv://dbUser:8g4NfIdmV1VCXpMM@scheduley-l3rwo.gcp.mongodb.net/test?retryWrites=true&w=majority";
+  const uri = MongoUri;
 
 
   const client = new MongoClient(uri);
@@ -132,14 +132,3 @@ async function listDatabases(client){
   console.log("Databases:");
   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
-
-// /**
-// * Print the names of all available databases
-// * @param {MongoClient} client A MongoClient that is connected to a cluster
-// */
-// async function listDatabases(client) {
-//   databasesList = await client.db().admin().listDatabases();
-
-//   console.log("Databases:");
-//   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-// };
